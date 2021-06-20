b8af5fdcdef68e40377055ca3e61187e273dcfb9
shimon tadesa <shimontadesa123@gmail.com>
2021-06-20 16:06:52 +0300

    get nasa key and add map component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import Map from "./component/Map";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
+    };
+    fetchEvent();
+    console.log(eventData);
+  }, []);
   return (
     <div className="App">
       <Map />
